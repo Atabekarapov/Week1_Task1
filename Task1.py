4e67a5db5095b457a300230f2ad5b845830a593d
Atabek <atabekarapov14@gmail.com>
2020-11-01 17:25:35 +0600

    fist

---
+++ Task1.py
@@ -0,0 +1,28 @@
+# #  Homework 
+# #  1) 
+# a = 5
+# b = 10 
+# c = 2
+# print (c*(a + b))
+
+
+# # 2)
+# x = 10
+# y = 3
+# print(x % y)
+
+# # 3)
+# word = input( "You_are_age: ")
+# print (word)
+
+
+
+
+# # 4)
+# a = 10
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a // b)
+
